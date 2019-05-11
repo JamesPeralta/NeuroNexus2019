a2acb6e87cd6d4a5f2a2c2b359ea925da9b741ac
jamesperalta <jamesperalta35@gmail.com>
2019-05-10 23:34:05 -0600

    Upload common functions

--- Custom-Libraries/Common.py
+++ Custom-Libraries/Common.py
@@ -30,10 +30,27 @@ def show_slice(image):
     plt.imshow(image)
 
 
-# ----------------------------------------------------------------------
-# Playing around with the functions
-location = "/Users/jamesperalta/Desktop/NeuroNexus/HC/HC/HC_T1.nii"
-scan = retrieve_scan(location)
-print(scan[0].shape)
+def show_images(images, cols=1, titles=None):
+    """
+    Display a list of images in a single figure with matplotlib.
 
-plt.imshow(scan[0])
+    Parameters
+    ---------
+    images: List of np.arrays compatible with plt.imshow.
+
+    cols (Default = 1): Number of columns in figure (number of rows is set to np.ceil(n_images/float(cols))).
+
+    titles: List of titles corresponding to each image. Must have the same length as titles.
+    """
+    assert ((titles is None) or (len(images) == len(titles)))
+    n_images = len(images)
+    if titles is None: titles = ['Image (%d)' % i for i in range(1, n_images + 1)]
+    fig = plt.figure()
+    for n, (image, title) in enumerate(zip(images, titles)):
+        a = fig.add_subplot(cols, np.ceil(n_images / float(cols)), n + 1)
+        if image.ndim == 2:
+            plt.gray()
+        plt.imshow(image)
+        a.set_title(title)
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
+    plt.show()
